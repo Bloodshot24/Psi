ff1a7a7c7d60400d423cc7abad81cd8473140307
Bastien Genova <bastien.genova@gmail.com>
2026-01-19 12:39:06 +0100

    Création du program principal et test du Main

---
+++ Psi/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Psi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Arthur le gros sac a foutre rempli de merde qui déborde ca en peut plus tellement il est gros");
+        }
+    }
+}
