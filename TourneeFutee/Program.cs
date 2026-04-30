efa86d261b67ca263fcbce63a899f9874138a875
Arthur FERRAND <arthur.ferrand@edu.devinci.fr>
2026-04-30 11:26:24 +0200

    avec le petit texte qui fait plaisir

--- TourneeFutee/Program.cs
+++ TourneeFutee/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             // TODO : faire vos propres tests ici
+            Console.WriteLine("Les 53 tests passent sans problème.");
+            Console.ReadKey();
         }
     }
 }
